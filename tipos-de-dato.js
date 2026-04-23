18977a4951c0cf96c0b6c3dbecfed40534e8d104
Luis Bernabe <juan1juan1juan1@outlook.es>
2026-04-22 23:11:06 -0600

    Introduccion a JS-TIPOS DE DATOS

---
+++ tipos-de-dato.js
@@ -0,0 +1,40 @@
+//Tipos de datos
+
+//String
+console.log(typeof 'Veinticinco');
+console.log(typeof '"');
+
+//Number
+console.log(typeof 42);
+console.log(typeof -666);
+console.log(typeof 0);
+
+
+//Boolean
+console.log(typeof false);
+
+
+//Undefined
+console.log(typeof undefined);
+
+
+//NULL
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
