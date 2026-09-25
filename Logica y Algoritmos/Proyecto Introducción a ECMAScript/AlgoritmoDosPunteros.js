74c66d403d7ea376c29358bdea1d89cb2cfc9d71
Luis Bernabe <juan1juan1juan1@outlook.es>
2026-09-24 18:08:13 -0600

    Agregando el algoritmo de dos punteros

---
+++ Logica y Algoritmos/Proyecto Introducción a ECMAScript/AlgoritmoDosPunteros.js
@@ -0,0 +1,31 @@
+const invitados = ["Ana", "Carlos", "Cecilia", "Daniel", "Diana", "Eduardo"];
+
+function encontrarPareja(arr) {
+    let inicio = 0;
+    let siguiente = 1;
+
+
+   while (siguiente < arr.length) {
+
+        // TODO: Compara las iniciales de los nombres en los punteros
+        // Si coinciden, devuelve el par
+    
+        if(arr[inicio][0] === arr[siguiente][0]){
+            return arr.filter(elemento => [arr[inicio], arr[siguiente]].includes(elemento))
+        }
+
+        
+        
+
+        // TODO: Avanza los punteros si no coinciden
+        // ...
+        inicio++;
+        siguiente++;
+
+    }
+
+    return null; // Si no se encuentra ningún par
+}
+
+console.log(encontrarPareja(invitados));
+// Resultado: ["Carlos", "Cecilia"]
